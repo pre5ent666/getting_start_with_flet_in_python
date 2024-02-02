2c6285085553e41c2a8c428f1b62d1215a1fd8db
pre5ent666 <75908533+pre5ent666@users.noreply.github.com>
2024-02-02 16:57:38 +0800

    added variables to store views and added some comments for learning
    Can we create each page separately and combine them together?

--- pages_navigation.py
+++ pages_navigation.py
@@ -6,27 +6,28 @@ def main(page: ft.Page):
     page.window_maximized = True
     page.title = "Routes Example"
 
-    def route_change(route):
-        page.views.clear()
-        page.views.append(
-            ft.View(
-                "/",
-                [
-                    ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
-                ],
-            )
-        )
-        if page.route == "/store":
-            page.views.append(
-                ft.View(
+    # To navigate between pages, use page.go(route) - a helper method that updates page.route
+    page_store = ft.View(
                     "/store",
                     [
                         ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
                         ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
                     ],
                 )
+    
+    page_home = ft.View(
+                "/",
+                [
+                    ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
+                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
+                ],
             )
+    
+    def route_change(route):
+        page.views.clear()
+        page.views.append(page_home)
+        if page.route == "/store":
+            page.views.append(page_store)
         page.update()
 
     def view_pop():
@@ -34,7 +35,9 @@ def main(page: ft.Page):
         top_view = page.views[-1]
         page.go(top_view.route)
 
+    # page.on_route_change event handler is a function of a route, navigation history stack (a list of views depending on the current route)
     page.on_route_change = route_change
+    # page.on_view_pop event handler, fires when the user clicks automatic "Back" button in AppBar control, remove the last element from views collection and navigate to view's root "under" it.
     page.on_view_pop = view_pop
     page.go(page.route)
 
